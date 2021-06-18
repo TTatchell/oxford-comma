acb864bc6da4a86d17731e00f51cf5e5b5f82ba4
TTatchell <tim.tatchell24@gmail.com>
2021-06-18 12:08:57 +1000

    upload

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,36 @@
-def oxford_comma(array)
+#array = ["kiwi"]
+array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
 
+def oxford_comma(array)
+    returnString = ""
+
+
+    if array.size == 1
+        returnString << array[0]
+    end
+
+    if array.size == 2
+        returnString = array[0] + " and " + array[1]
+    end
+
+
+
+    if array.size > 2
+
+        array.each_with_index do |fruit, index|
+            if index == array.size - 1
+                returnString << ", and " + fruit
+                next
+
+            elsif index != 0
+                returnString << ", " + fruit
+                next
+            else
+                returnString << fruit
+            end
+        end
+    end
+    returnString
 end
+
+puts oxford_comma(array)
